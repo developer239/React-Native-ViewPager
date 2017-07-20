b45d60ff5d194578538f0eda39ccc50d6af80357
michaljarnot <michal.jarnot@strv.com>
2017-07-20 02:24:47 +0200

    Change indicator page viewer data flow

--- viewpager/indicator/PagerDotIndicator.js
+++ viewpager/indicator/PagerDotIndicator.js
@@ -10,77 +10,85 @@ import IndicatorViewPager from '../IndicatorViewPager'
 
 const DEFAULT_DOT_RADIUS = 6
 export default class PagerDotIndicator extends Component {
-    static propTypes = {
-        ...View.propTypes,
-        pageCount: PropTypes.number.isRequired,
-        initialPage: PropTypes.number,
-        pager: PropTypes.instanceOf(IndicatorViewPager),
-        dotStyle: View.propTypes.style,
-        selectedDotStyle: View.propTypes.style,
-        hideSingle: React.PropTypes.bool
-    }
+  static propTypes = {
+    ...View.propTypes,
+    pageCount: PropTypes.number.isRequired,
+    initialPage: PropTypes.number,
+    pager: PropTypes.instanceOf(IndicatorViewPager),
+    dotStyle: View.propTypes.style,
+    selectedDotStyle: View.propTypes.style,
+    hideSingle: React.PropTypes.bool
+  }
 
-    static defaultProps = {
-        pageCount: 0,
-        initialPage: 0,
-        hideSingle: false
-    }
+  static defaultProps = {
+    pageCount: 0,
+    initialPage: 0,
+    hideSingle: false
+  }
 
-    state = {
-        selectedIndex: this.props.initialPage
-    }
+  state = {
+    selectedIndex: this.props.initialPage
+  }
 
-    shouldComponentUpdate (nextProps, nextState) {
-        return this.state.selectedIndex != nextState.selectedIndex ||
-            this.props.pageCount != nextProps.pageCount ||
-            this.props.dotStyle != nextProps.dotStyle ||
-            this.props.selectedDotStyle != nextProps.selectedDotStyle ||
-            this.props.style != nextProps.style
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.selectedIndex != nextState.selectedIndex ||
+      this.props.pageCount != nextProps.pageCount ||
+      this.props.dotStyle != nextProps.dotStyle ||
+      this.props.selectedDotStyle != nextProps.selectedDotStyle ||
+      this.props.style != nextProps.style
+  }
 
-    render() {
-        let {pageCount, dotStyle, selectedDotStyle}=this.props;
-        if (pageCount <= 0)return null;
-        if (this.props.hideSingle && pageCount == 1) return null;
-        let dotsView = [];
-        for (let i = 0; i < pageCount; i++) {
-            let isSelect = i === this.state.selectedIndex
-            dotsView.push(
-                <View
-                    style={[styles.dot, isSelect ? styles.selectDot : null, isSelect ? selectedDotStyle : dotStyle]}
-                    key={i}
-                />
-            )
-        }
-        return (
-            <View {...this.props} style={[styles.container, this.props.style]} >
-                {dotsView}
-            </View>
-        )
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.initialPage !== this.state.selectedIndex) {
+      this.setState({
+        selectedIndex: nextProps.initialPage
+      })
     }
+  }
 
-    onPageSelected (e) {
-        this.setState({selectedIndex: e.position})
+  render() {
+    let { pageCount, dotStyle, selectedDotStyle } = this.props;
+    if (pageCount <= 0)return null;
+    if (this.props.hideSingle && pageCount == 1) return null;
+    let dotsView = [];
+    for (let i = 0; i < pageCount; i++) {
+      let isSelect = i === this.state.selectedIndex
+      dotsView.push(
+        <View
+          style={[styles.dot, isSelect ? styles.selectDot : null, isSelect ? selectedDotStyle : dotStyle]}
+          key={i}
+        />
+      )
     }
+    return (
+      <View {...this.props} style={[styles.container, this.props.style]}>
+        {dotsView}
+      </View>
+    )
+  }
+
+  onPageSelected(e) {
+    this.setState({ selectedIndex: e.position })
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        bottom: 10,
-        left: 0,
-        right: 0,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    dot: {
-        width: DEFAULT_DOT_RADIUS,
-        height: DEFAULT_DOT_RADIUS,
-        borderRadius: DEFAULT_DOT_RADIUS >> 1,
-        backgroundColor: '#BBBBBB',
-        margin: DEFAULT_DOT_RADIUS >> 1
-    },
-    selectDot: {
-        backgroundColor: 'white'
-    }
+  container: {
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  dot: {
+    width: DEFAULT_DOT_RADIUS,
+    height: DEFAULT_DOT_RADIUS,
+    borderRadius: DEFAULT_DOT_RADIUS >> 1,
+    backgroundColor: '#BBBBBB',
+    margin: DEFAULT_DOT_RADIUS >> 1
+  },
+  selectDot: {
+    backgroundColor: 'white'
+  }
 })
